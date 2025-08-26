ada677681890b890b41e5a1b329413a1cb437b93
ExgleGaming <EaglesFan91x@outlook.com>
2025-08-25 22:24:46 -0400

    update to player movement

--- scripts/game_object/player/player.gd
+++ scripts/game_object/player/player.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(GameConstants.JUMP) and is_on_floor():
 		velocity.y = jump_velocity
 
-	var direction := Input.get_axis(GameConstants.MOVE_LEFT, GameConstants.MOVE_RIGHT)
+	var direction: float = Input.get_axis(GameConstants.MOVE_LEFT, GameConstants.MOVE_RIGHT)
 	if direction:
 		velocity.x = direction * speed
 	else:
