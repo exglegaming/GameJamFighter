7c9ea12bf8db129e46834f8c35f05905969a8a7b
hohe1 <anthonyho02015@gmail.com>
2025-08-26 14:31:17 -0700

    added collection component

---
+++ scripts/components/collection_component.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _on_body_entered(body):
+	print(global_position)
+	body.apply_central_impulse(Vector2.UP*250)
+	body.apply_central_impulse((global_position - body.position).normalized()*100)
+	
+func _on_inner_area_body_entered(body):
+	body.queue_free()
+
+#func _on_body_exited(body):
