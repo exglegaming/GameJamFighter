9d85ee8caab9a989d2d26ea245baad74736f8631
ExgleGaming <EaglesFan91x@outlook.com>
2025-08-26 21:50:03 -0400

    fixed an issue with player being blurry on movement

--- scripts/game_object/game_camera/game_camera.gd
+++ scripts/game_object/game_camera/game_camera.gd
@@ -1,13 +1,16 @@
 extends Camera2D
 
 var target_position: Vector2 = Vector2.ZERO
+
 @onready var ui = get_node("Control")
-func _physics_process(delta: float) -> void:
+
+
+func _process(delta: float) -> void:
 	acquire_target()
 	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 20))
 
 func acquire_target() -> void:
-	var player_nodes: Array[Node] = get_tree().get_nodes_in_group("player")
+	var player_nodes := get_tree().get_nodes_in_group("player")
 	if player_nodes.size() > 0:
 		var player: Node2D = player_nodes[0]
 		target_position = player.global_position
